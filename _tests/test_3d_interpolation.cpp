966734207a44ac156e908095fc989a65e9b14743
Otavio Pellicano <otavio.pellicano@gmail.com>
2020-06-06 21:41:43 -0300

    Enable Vertex to accept angle as deg
    fb-SPL-2

--- _tests/test_3d_interpolation.cpp
+++ _tests/test_3d_interpolation.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE test_3d_interpolation
 #include <boost/test/included/unit_test.hpp>
+namespace utf = boost::unit_test;
 
 #include <map>
 #include <Base3DInterpolation.hpp>
@@ -9,8 +10,8 @@ std::string message_error_vertices_compare(const Vertex& v_1, const Vertex& v_2)
 {
     using namespace std;
 
-    return "{" + to_string(v_1.curve_length) + ", " + to_string(v_1.inclination) + ", " + to_string(v_1.azimuth) + "} != "
-           "{" + to_string(v_2.curve_length) + ", " + to_string(v_2.inclination) + ", " + to_string(v_2.azimuth) + "}";
+    return "{" + to_string(v_1.curve_length()) + ", " + to_string(v_1.inclination()) + ", " + to_string(v_1.azimuth()) + "} != "
+           "{" + to_string(v_2.curve_length()) + ", " + to_string(v_2.inclination()) + ", " + to_string(v_2.azimuth()) + "}";
 }
 
 BOOST_AUTO_TEST_CASE( test_calculate_adjacent_vertices )
@@ -40,5 +41,27 @@ BOOST_AUTO_TEST_CASE( test_calculate_adjacent_vertices )
         BOOST_TEST(adjacent_vertices.first.approx_equal(item.second.first), message_error_vertices_compare(adjacent_vertices.first, item.second.first));
         BOOST_TEST(adjacent_vertices.second.approx_equal(item.second.second), message_error_vertices_compare(adjacent_vertices.second, item.second.second));
     }
+
+}
+
+BOOST_AUTO_TEST_CASE( test_angle_conversion , * utf::tolerance(1E-6))
+{
+
+    Vertices vertices =
+    {
+        {0.5, M_PI / 2, M_PI, AngleType::rad},
+        {1.0, 45.0, 30.0, AngleType::deg}
+    };
+
+    BOOST_TEST(vertices[0].inclination() == M_PI / 2);
+    BOOST_TEST(vertices[0].azimuth() == M_PI);
+
+    vertices[0].set_angle_type(AngleType::deg);
+    BOOST_TEST(vertices[0].inclination() == 90.0);
+    BOOST_TEST(vertices[0].azimuth() == 180.0);
+
+    vertices[1].set_angle_type(AngleType::rad);
+    BOOST_TEST(vertices[1].inclination() == M_PI / 4);
+    BOOST_TEST(vertices[1].azimuth() == M_PI / 6);
 
 }

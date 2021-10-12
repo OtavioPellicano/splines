1e96a43fcf880bebcef43c321838d60d36d99146
Otavio Pellicano <otavio.pellicano@gmail.com>
2021-10-12 14:50:52 -0300

    Add black fix format

--- interpolator/python/_tests/test_3d_interpolation.py
+++ interpolator/python/_tests/test_3d_interpolation.py
@@ -1,20 +1,40 @@
-from _interpolator import (AngleUnit, BuildInterpolator,
-                           InterpolationType, Vertex, Trajectory)
+from _interpolator import (
+    AngleUnit,
+    BuildInterpolator,
+    InterpolationType,
+    Vertex,
+    Trajectory,
+)
 import pytest
 import numpy as np
 
 
 @pytest.fixture
 def trajectory_SPE84246():
-    return Trajectory([Vertex(214.13724, 0.095993095, 0.785398049999999),
-                Vertex(598.800936, 0.519235377499999, 1.3447759945),
-                Vertex(1550.31948, 0.519235377499999, 1.3447759945),
-                Vertex(3018.032064, 2.09439479999999, 4.97418765)])
+    return Trajectory(
+        [
+            Vertex(214.13724, 0.095993095, 0.785398049999999),
+            Vertex(598.800936, 0.519235377499999, 1.3447759945),
+            Vertex(1550.31948, 0.519235377499999, 1.3447759945),
+            Vertex(3018.032064, 2.09439479999999, 4.97418765),
+        ]
+    )
 
 
 @pytest.fixture
 def interpolation_samples_SPE84246():
-    return sorted([214.13724, 598.800936, 1550.31948, 3018.032064, 1295.4, 2592.052728, 2690.786592, 2789.520456])
+    return sorted(
+        [
+            214.13724,
+            598.800936,
+            1550.31948,
+            3018.032064,
+            1295.4,
+            2592.052728,
+            2690.786592,
+            2789.520456,
+        ]
+    )
 
 
 def _CompareInterpolationTypeStr(interpolator):
@@ -30,21 +50,23 @@ def _CompareInterpolationTypeStr(interpolator):
 
 
 @pytest.mark.parametrize(
-    'angle_unit',
-    [AngleUnit.Deg, AngleUnit.Rad],
-    ids=['deg', 'rad'])
+    "angle_unit", [AngleUnit.Deg, AngleUnit.Rad], ids=["deg", "rad"]
+)
 @pytest.mark.parametrize(
-    'interpolation_type',
-    [InterpolationType.Linear,
-     InterpolationType.MinimumCurvature,
-     InterpolationType.Cubic],
-    ids=['linear', 'minimum_curvature', 'cubic'])
+    "interpolation_type",
+    [
+        InterpolationType.Linear,
+        InterpolationType.MinimumCurvature,
+        InterpolationType.Cubic,
+    ],
+    ids=["linear", "minimum_curvature", "cubic"],
+)
 def test_3d_interpolation(
-        num_regression,
-        trajectory_SPE84246,
-        interpolation_samples_SPE84246,
-        angle_unit,
-        interpolation_type
+    num_regression,
+    trajectory_SPE84246,
+    interpolation_samples_SPE84246,
+    angle_unit,
+    interpolation_type,
 ):
 
     trajectory = trajectory_SPE84246
@@ -70,29 +92,32 @@ def test_3d_interpolation(
         projections_z[i] = interpolator.ZAtPosition(sample)
 
     res = dict()
-    res['position'] = positions
-    res['inclination'] = inclinations
-    res['azimuth'] = azimuths
-    res['projection_x'] = projections_x
-    res['projection_y'] = projections_y
-    res['projection_z'] = projections_z
+    res["position"] = positions
+    res["inclination"] = inclinations
+    res["azimuth"] = azimuths
+    res["projection_x"] = projections_x
+    res["projection_y"] = projections_y
+    res["projection_z"] = projections_z
 
     num_regression.check(res)
 
 
 @pytest.mark.parametrize(
-    'delimiter',
-    [',', ';', '|', '-'],
-    ids=['default(comma)', 'semicolon', 'pipe', 'dash'])
+    "delimiter",
+    [",", ";", "|", "-"],
+    ids=["default(comma)", "semicolon", "pipe", "dash"],
+)
 def test_vertex_repr_operator(delimiter):
     vertex = Vertex(1.1, 2.2, 3.3)
 
     # Default delimiter: comma
-    if delimiter != ',':
+    if delimiter != ",":
         vertex.SetDelimiter(delimiter)
 
     assert delimiter == vertex.Delimiter()
-    assert str(vertex) == delimiter.join([str(vertex.Position()), str(vertex.Inclination()), str(vertex.Azimuth())])
+    assert str(vertex) == delimiter.join(
+        [str(vertex.Position()), str(vertex.Inclination()), str(vertex.Azimuth())]
+    )
 
 
 # TODO: SPL-77 add SetVertices test
@@ -103,10 +128,12 @@ def test_trajectory_class(trajectory_SPE84246):
 
     for vertex in trajectory_SPE84246.Vertices():
         trajectory_deg.append(
-            Vertex(vertex.Position(),
-                   vertex.Inclination(AngleUnit.Deg),
-                   vertex.Azimuth(AngleUnit.Deg),
-                   AngleUnit.Deg)
+            Vertex(
+                vertex.Position(),
+                vertex.Inclination(AngleUnit.Deg),
+                vertex.Azimuth(AngleUnit.Deg),
+                AngleUnit.Deg,
+            )
         )
 
     def compare_trajectory(t_1, t_2):
@@ -116,8 +143,12 @@ def test_trajectory_class(trajectory_SPE84246):
             assert pytest.approx(vt_1.Position(), vt_2.Position())
             assert pytest.approx(vt_1.Inclination(), vt_2.Inclination())
             assert pytest.approx(vt_1.Azimuth(), vt_2.Azimuth())
-            assert pytest.approx(vt_1.Inclination(AngleUnit.Deg), vt_2.Inclination(AngleUnit.Deg))
-            assert pytest.approx(vt_1.Azimuth(AngleUnit.Deg), vt_2.Azimuth(AngleUnit.Deg))
+            assert pytest.approx(
+                vt_1.Inclination(AngleUnit.Deg), vt_2.Inclination(AngleUnit.Deg)
+            )
+            assert pytest.approx(
+                vt_1.Azimuth(AngleUnit.Deg), vt_2.Azimuth(AngleUnit.Deg)
+            )
 
     trajectory_set_sorted = Trajectory()
     trajectory_set = Trajectory()
